beef03afadd4ea0cf496dd6b37b0fdd8ea93c010
JavideuS <javi.rm2005@gmail.com>
2024-09-22 13:44:08 +0200

    Rasterization/fill algorithm

--- star.jl
+++ star.jl
@@ -6,13 +6,12 @@ using LinearAlgebra
 #Characters classified by luminance
 luminance = "@\$#*!=;:~-,."
 
-
 #Terminal size -> (heigth,width)
 terminal_size = displaysize(stdout)
 #Center
-center = round.(Int16,[terminal_size[1]/2 ,terminal_size[2]/2 ])
+center = round.(Int16,[terminal_size[2]/2 ,terminal_size[1]/2 ])
 #Big radius
-R = center[1]  
+R = center[2]  
 
 #Star outer points (edges)
 #Setting them to 0.0 since I will use all the terminal for calculations and just round it when printing the point
@@ -36,55 +35,10 @@ for i in 0:4
    inner_points[i+1] = [r * cosd(current_θ), r * sind(current_θ),0]
 end
 
-#Now that we have the inner and outer points, now we can define the points for each triangle
-#Each triangle will safe its each 3 points
-outer_triangles = [[[0.0, 0.0] for _ in 1:3] for _ in 1:5]
-for i in 1:5
-   outer_triangles[i][1][2] = outer_points[i][2]
-   outer_triangles[i][1][1] = outer_points[i][1]
-   for j in 0:1
-      outer_triangles[i][2+j][1] = inner_points[((i+ j - 1) % 5) + 1][1] 
-      outer_triangles[i][2+j][2] = inner_points[((i+j - 1) % 5) + 1][2]
-   end
-end
-
-#Now we define the inner triangles which are combinations 
-inner_triangles = [[[0.0, 0.0] for _ in 1:3] for _ in 1:3]
-for i in 1:3
-   inner_triangles[i][1] = [inner_points[2i-1]...]
-   inner_triangles[i][2] = [inner_points[2i%5]...]
-   inner_triangles[i][3] = [inner_points[(i - 1) % 2 == 0 ? 3 : 5]...] 
-end
-
-star_triangles = [outer_triangles...,inner_triangles...]
-
-function cross2d(v1, v2)
-   if size(v1) != size(v2) != 2
-      throw(ArgumentError("Vector must have exactly 2 elements"))
-   end
-   return v1[1] * v2[2] - v1[2] * v2[1]
-end
-
-function rasterizeTriangle(point::Vector{Int16})
-   for i in 1:8
-      # AP x AB
-      first_edge = cross2d((point - (center .- star_triangles[i][1])),((center - star_triangles[i][2]) - (center .- star_triangles[i][1]))) 
-      #BP x BC
-      second_edge = cross2d((point - (center .- star_triangles[i][2])),((center - star_triangles[i][3]) - (center .- star_triangles[i][2])))  
-      #CP x cA
-      third_edge = cross2d((point - (center .- star_triangles[i][3])),((center - star_triangles[i][1]) - (center .- star_triangles[i][3]))) 
-
-      if (first_edge >= 0  && second_edge >= 0 && third_edge >=0 ) || (first_edge <= 0  && second_edge <= 0 && third_edge <= 0)
-         frame[point...] = '#'
-         break
-      end
-   end
-end
-
 #Then I define the range of my star in the terminal_matrix
 #This will just help reduce the points to rasterize/fill
-x_range = round(Int16,(center[2] + outer_points[2][2])):round(Int16,(center[2] + outer_points[5][2]))
-#The the star will use all the height so the y_range doesnt matter
+x_range = round(Int16,(center[1] + outer_points[2][1])):round(Int16,(center[1] + outer_points[5][1]))
+#The star will use all the height so the y_range doesnt matter
 
 #RGB code colors
 light_red, medium_light_red, medium_red, medium_dark_red, dark_red = 211, 204, 196, 160, 88  
@@ -110,18 +64,19 @@ for i in 1:star_depth-1
 end
 
 
-k1= center[1]*distance*3/4((R+r))
+star_triangles = [[[[0,0] for _ in 1:3] for _ in 1:8] for _ in 1:star_depth]
+
+k1= center[2]*distance*3/4((R+r))
 function update_frame()
-   for depth in vertex_grid
-      for points in depth
+   for (z,depth) in enumerate(vertex_grid)
+      a = [Int16.([0,0]) for _ in 1:10]
+      for (y,points) in enumerate(depth)
          #It is formatted (y,x)
          ooz = 1/(distance + points[3])
          x′ = k1 * points[1] * ooz
          y′ = k1 * points[2] * ooz
-         pos = round.(Int16,[center[1] - y′,center[2] + x′])
-         if pos[1] < 1
-            pos[1] = 1
-         end
+         pos = round.(Int16,[center[2] - y′,center[1] + x′])
+         a[y] = [pos[2],pos[1]]
          ilumin_index = round(Int16,points[3]+1)
          if ilumin_index < 1
             ilumin_index = 1
@@ -129,6 +84,53 @@ function update_frame()
             ilumin_index = 12
          end
          frame[pos...] = luminance[ilumin_index]
+      end
+      star_triangles[z] = update_triangle(a[1:5],a[6:10])
+   end
+end
+
+#Now that we have the inner and outer points, now we can define the points for each triangle
+#Each triangle will safe its each 3 points
+function update_triangle(ext_points,int_points)
+   xd = [[[0,0] for _ in 1:3] for _ in 1:8]
+   for i in 1:5
+      xd[i][1] = ext_points[i]
+      for j in 0:1
+          xd[i][2+j] = int_points[((i + j - 1) % 5) + 1]
+      end
+  end
+
+   for i in 1:3
+      xd[i+5][1] = [int_points[2i-1]...]
+      xd[i+5][2] = [int_points[2i%5]...]
+      xd[i+5][3] = [int_points[(i - 1) % 2 == 0 ? 3 : 5]...] 
+   end
+   return xd
+end
+
+
+function cross2d(v1, v2)
+   if size(v1) != size(v2) != 2
+      throw(ArgumentError("Vector must have exactly 2 elements"))
+   end
+   return v1[1] * v2[2] - v1[2] * v2[1]
+end
+
+function rasterizeTriangle(point::Vector{Int16})
+   for i in 1:star_depth
+      for j in 1:8
+         # AP x AB
+         alt_points = [point[2],point[1]]
+         first_edge = cross2d((alt_points -  star_triangles[i][j][1]),(star_triangles[i][j][2] - star_triangles[i][j][1])) 
+         #BP x BC
+         second_edge = cross2d((alt_points -  star_triangles[i][j][2]),(star_triangles[i][j][3] - star_triangles[i][j][2]))  
+         #CP x cA
+         third_edge = cross2d((alt_points - star_triangles[i][j][3]),(star_triangles[i][j][1] - star_triangles[i][j][3])) 
+
+         if (first_edge >= 0  && second_edge >= 0 && third_edge >=0 ) || (first_edge <= 0  && second_edge <= 0 && third_edge <= 0)
+            frame[point...] = '#'
+            break
+         end
       end
    end
 end
@@ -160,15 +162,16 @@ z_rotation = [cos(δ) sin(δ) 0;
 
                 
 
-# for i in 1:terminal_size[1]
-#    for j in (center[2] + outer_points[2][2]):terminal_size[2]
-#      rasterizeTriangle(Int16.([i, j]))
-#    end
-# end
+
 while(true)
    print("\x1b[H") #Prints at the top left line
    frame = fill(' ', terminal_size)
    update_frame()
+   for i in 1:terminal_size[1]
+      for j in x_range
+        rasterizeTriangle(round.(Int16,[i, j]))
+      end
+   end
    print_frame(frame, terminal_size[1],terminal_size[2])
    for (i,depth) in enumerate(vertex_grid)
       for (j,points) in enumerate(depth)               
