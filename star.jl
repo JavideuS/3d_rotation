8e87ed5e72f582e3f2be12cbd3388dfbb22908a8
JavideuS <javi.rm2005@gmail.com>
2024-09-23 22:37:13 +0200

    Star with depth_buffer -> Somewhat better

--- star.jl
+++ star.jl
@@ -4,7 +4,8 @@ using LinearAlgebra
 #Angles
 α,β,δ = π/8, π/8, π/8
 #Characters classified by luminance
-luminance = "@\$#*!=;:~-,."
+#luminance = "@\$#*!=;:~-,."
+luminance = ".,-~:;=!*#\$@"
 
 #Terminal size -> (heigth,width)
 terminal_size = displaysize(stdout)
@@ -63,13 +64,14 @@ for i in 1:star_depth-1
    end
 end
 
-
 star_triangles = [[[[0,0] for _ in 1:3] for _ in 1:8] for _ in 1:star_depth]
+norms = [[0 for _ in 1:8] for _ in 1:star_depth]
 
 k1= center[2]*distance*3/4((R+r))
 function update_frame()
    for (z,depth) in enumerate(vertex_grid)
       a = [Int16.([0,0]) for _ in 1:10]
+      norms[z] = calculate_luminance(get_triangles(depth[1:5],depth[6:10],3))
       for (y,points) in enumerate(depth)
          #It is formatted (y,x)
          ooz = 1/(distance + points[3])
@@ -77,22 +79,21 @@ function update_frame()
          y′ = k1 * points[2] * ooz
          pos = round.(Int16,[center[2] - y′,center[1] + x′])
          a[y] = [pos[2],pos[1]]
-         ilumin_index = round(Int16,points[3]+1)
-         if ilumin_index < 1
-            ilumin_index = 1
-         elseif  ilumin_index > 12
-            ilumin_index = 12
-         end
-         frame[pos...] = luminance[ilumin_index]
       end
-      star_triangles[z] = update_triangle(a[1:5],a[6:10])
+      star_triangles[z] = get_triangles(a[1:5],a[6:10],2)
    end
 end
 
 #Now that we have the inner and outer points, now we can define the points for each triangle
 #Each triangle will safe its each 3 points
-function update_triangle(ext_points,int_points)
-   xd = [[[0,0] for _ in 1:3] for _ in 1:8]
+function get_triangles(ext_points,int_points,dimension)
+   if dimension == 2
+      xd = [[[0,0] for _ in 1:3] for _ in 1:8]
+   elseif dimension == 3
+      xd = [[[0.0,0.0,0.0] for _ in 1:3] for _ in 1:8]
+   else
+      return -1
+   end
    for i in 1:5
       xd[i][1] = ext_points[i]
       for j in 0:1
@@ -106,6 +107,39 @@ function update_triangle(ext_points,int_points)
       xd[i+5][3] = [int_points[(i - 1) % 2 == 0 ? 3 : 5]...] 
    end
    return xd
+end
+
+function calculate_luminance(triangles)
+
+   light_value = [0 for _ in 1:8]
+   for i in 1:8
+      A = triangles[i][1]
+      B = triangles[i][2]
+      C = triangles[i][3]
+
+      # Step 1: Calculate edges
+      edge1 = (B[1] - A[1], B[2] - A[2], B[3] - A[3])  # B - A
+      edge2 = (C[1] - A[1], C[2] - A[2], C[3] - A[3])  # C - A
+
+      normal = (
+        edge1[2] * edge2[3] - edge1[3] * edge2[2],  # i-component
+        edge1[3] * edge2[1] - edge1[1] * edge2[3],  # j-component
+        edge1[1] * edge2[2] - edge1[2] * edge2[1]   # k-component
+    )
+
+      norm_length = sqrt(normal[1]^2 + normal[2]^2 + normal[3]^2)
+      normalized_normal = (normal[1] / norm_length, normal[2] / norm_length, normal[3] / norm_length)
+      light_direction = normalize([0, 0, -1])  # Light coming from in front
+      dot_product = dot(normalized_normal, light_direction)  # Dot product of normal and light direction
+      luminance_value = (dot_product + 1) / 2  # Normalize from [-1, 1] to [0, 1]
+
+      # Convert luminance_value to an index
+      char_index = round(Int, luminance_value * (length(luminance) - 1))
+
+      # Get the corresponding character
+      light_value[i] = char_index + 1
+   end
+   return light_value
 end
 
 
@@ -128,7 +162,9 @@ function rasterizeTriangle(point::Vector{Int16})
          third_edge = cross2d((alt_points - star_triangles[i][j][3]),(star_triangles[i][j][1] - star_triangles[i][j][3])) 
 
          if (first_edge >= 0  && second_edge >= 0 && third_edge >=0 ) || (first_edge <= 0  && second_edge <= 0 && third_edge <= 0)
-            frame[point...] = '#'
+            if frame[point...] == ' ' || norms[i][j] > findfirst(frame[point...],luminance)
+               frame[point...] = luminance[norms[i][j]]
+            end
             break
          end
       end
@@ -159,9 +195,6 @@ y_rotation = [cos(β) 0 sin(β);
 z_rotation = [cos(δ) sin(δ) 0;
              -sin(δ) cos(δ) 0;
                 0      0    1]
-
-                
-
 
 while(true)
    print("\x1b[H") #Prints at the top left line
