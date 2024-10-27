0629f51f0c9541370387a980bbd2e95714ab8c74
JavideuS <javi.rm2005@gmail.com>
2024-10-27 14:15:30 +0100

    Cleaner code

--- star.jl
+++ star.jl
@@ -2,12 +2,11 @@
 using LinearAlgebra
 
 #Angles
-α,β,δ = π/8, π/6, π/8
+const α,β,δ = π/8, π/6, π/8
 #Characters classified by luminance
-#luminance = "@\$#*!=;:~-,."
-luminance = ".,-~:;=!*#\$@"
+const luminance = ".,-~:;=!*#\$@"
 
-yellow_palette = [
+const yellow_palette = [
     "\x1b[38;5;94m",   # Dark yellow
     "\x1b[38;5;100m",  # Darker golden
     "\x1b[38;5;136m",  # Gold
@@ -26,8 +25,16 @@ yellow_palette = [
 terminal_size = displaysize(stdout)
 #Center
 center = round.(Int16,[terminal_size[2]/2 ,terminal_size[1]/2 ])
-#Big radius
-R = center[2]  
+
+
+R=1
+#It takes the smaller axis
+if center[1] > center[2]
+   #Big radius
+   R = center[2]  
+else
+   R = center[1]
+end
 
 #Star outer points (edges)
 #Setting them to 0.0 since I will use all the terminal for calculations and just round it when printing the point
@@ -36,14 +43,17 @@ outer_points = fill([0.0,0.0,0.0],5)
 
 θ = 72 #360/5
 
+#Starting at 90 degrees so the first point is in the positive y axis
 for i in 0:4
    current_θ = 90 + (i * θ)
    outer_points[i + 1] = [R* cosd(current_θ), R * sind(current_θ),0]
 end
 
-#Small radius
+#Small radius 
 r = R/2
-#Each edge will have to corresponding inner points (But since each triangle share a point) it will only save half
+#Every outer points is supposed to have two more inner points, however, every triangles shares the inner point with other triangle
+#So technically we only need to use 5 inner points 
+
 inner_points = [[0.0, 0.0,0.0] for _ in 1:5] 
 
 for i in 0:4
@@ -79,12 +89,6 @@ star_triangles = [[[[0,0,0.0] for _ in 1:3] for _ in 1:8] for _ in 1:star_depth]
 
 k1= center[2]*distance*3/2.5((R+r))
 
-function update_frame()
-   for (z,vertex) in enumerate(vertex_grid)
-      star_triangles[z] = get_triangles(vertex[1:5],vertex[6:10])
-   end
-end
-
 # Perspective Projection with depth influence
 function project_point(point::Vector{Float64}, distance::Float64, center::Vector{Int16}, k1::Float64)
    ooz = 1 / (distance + point[3])  # Z-depth projection (inverse of distance)
@@ -95,37 +99,42 @@ function project_point(point::Vector{Float64}, distance::Float64, center::Vector
    return [x_proj, y_proj, point[3]]  # Projected X, Y and Z (for Z-buffer)
 end
 
-
 #Now that we have the inner and outer points, now we can define the points for each triangle
 #Each triangle will safe its each 3 points
 function get_triangles(ext_points,int_points)
 
-   xd = [[[0.0,0.0,0.0] for _ in 1:3] for _ in 1:8]
+   #There are in total 8 triangles
+   triangles = [[[0.0,0.0,0.0] for _ in 1:3] for _ in 1:8]
+
+   #Five are what I call outer triangles
    for i in 1:5
-      xd[i][1] = ext_points[i]
+      triangles[i][1] = ext_points[i]
       for j in 0:1
-          xd[i][2+j] = int_points[((i + j - 1) % 5) + 1]
+          triangles[i][2+j] = int_points[((i + j - 1) % 5) + 1]
       end
   end
 
+  #The last three is what I considered as inner triangles
    for i in 1:3
-      xd[i+5][1] = [int_points[2i-1]...]
-      xd[i+5][2] = [int_points[2i%5]...]
-      xd[i+5][3] = [int_points[(i - 1) % 2 == 0 ? 3 : 5]...] 
+      triangles[i+5][1] = [int_points[2i-1]...]
+      triangles[i+5][2] = [int_points[2i%5]...]
+      triangles[i+5][3] = [int_points[(i - 1) % 2 == 0 ? 3 : 5]...] 
    end
-   tri = copy(xd)
-   return tri
+   return triangles
 end
 
 function calculate_luminance(normal::Vector{Float64})
-   light_direction = normalize([0.0, -1.0, -1.0])  # Light direction
+   light_direction = normalize([0.0, -1.0, -1.0])  # Light direction (From below)
+   
    luminance_value = dot(normal, light_direction)  # Dot product of normal and light direction
 
    # Normalize luminance value to range [0, 1]
    luminance_value = (luminance_value + 1) / 2
 
    # Convert luminance value to character index
-   char_index = clamp(round(Int, luminance_value * (length(luminance) - 1)), 1, length(luminance))
+   #We have a value between 0 and 1, so we multiply it by the number of characters and the round it to the nearest int
+   #That way we get the index
+   char_index = round(Int, luminance_value * (length(luminance) - 1))
 
    return char_index
 end
@@ -139,27 +148,12 @@ function project_point(point::Vector{Float64}, distance::Float64, center::Vector
 end
 
 function apply_color_and_char(lum_index::Int)
-   # Map luminance to yellow shades
-   lum = ".,-~:;=!*#\$@"
 
-   colors = [
-    "\x1b[38;5;94m",   # Dark yellow
-    "\x1b[38;5;100m",  # Darker golden
-    "\x1b[38;5;136m",  # Gold
-    "\x1b[38;5;142m",  # Goldenrod
-    "\x1b[38;5;178m",  # Light goldenrod
-    "\x1b[38;5;184m",  # Yellowish
-    "\x1b[38;5;220m",  # Yellow-orange
-    "\x1b[38;5;226m",  # Bright yellow
-    "\x1b[38;5;227m",  # Pale yellow
-    "\x1b[38;5;228m",  # Lighter yellow
-    "\x1b[38;5;229m",  # Almost white-yellow
-    "\x1b[38;5;15m"    # White (brightest)
-]
-   
    # Get color and character for the current lum_index
-   color = colors[clamp(lum_index, 1, length(colors))]
-   char = lum[clamp(lum_index, 1, length(lum))]
+   #sometimes it can get to 0 and cause index error
+   #clamp function is to avoid that
+   color = yellow_palette[clamp(lum_index, 1, length(yellow_palette))]
+   char = luminance[clamp(lum_index, 1, length(luminance))]
    
    # Return the combined color code and character as a string
    return "$(color)$(char)\033[0m"  # Reset color with \033[0m
@@ -181,17 +175,13 @@ function rasterizeTriangle(p::Vector{Int16})
          # Screen space point being rasterized (alt_points)
          alt_points = [p[2], p[1]]  # Using p... for dynamic matrix access
 
-         #To show vertex better
-         # if alt_points == [A_proj[1],A_proj[2]] || alt_points == [B_proj[1],B_proj[2]] || alt_points == [C_proj[1],C_proj[2]]
-         #    frame[p...] = '#'
-         #    break 
-         # end
-
          # Calculate barycentric weights using 2D projected points
          denom = (B_proj[2] - C_proj[2]) * (A_proj[1] - C_proj[1]) + (C_proj[1] - B_proj[1]) * (A_proj[2] - C_proj[2])
          w1 = ((B_proj[2] - C_proj[2]) * (alt_points[1] - C_proj[1]) + (C_proj[1] - B_proj[1]) * (alt_points[2] - C_proj[2])) / denom
          w2 = ((C_proj[2] - A_proj[2]) * (alt_points[1] - C_proj[1]) + (A_proj[1] - C_proj[1]) * (alt_points[2] - C_proj[2])) / denom
+         #Since all weights should add up to 1, we just need to calculate to of them
          w3 = 1 - w1 - w2
+         #If every weight is positive, then the point is int he right side of the triangle
          if w1 >= 0 && w2 >= 0 && w3 >= 0  # Point inside the triangle
             # Interpolate depth in 3D space using barycentric coordinates
             z = w1 * A_3D[3] + w2 * B_3D[3] + w3 * C_3D[3]
@@ -212,10 +202,11 @@ function rasterizeTriangle(p::Vector{Int16})
    end
 end
 
-
-
-
-
+function update_frame()
+   for (z,vertex) in enumerate(vertex_grid)
+      star_triangles[z] = get_triangles(vertex[1:5],vertex[6:10])
+   end
+end
 
 function print_frame(frame::Array,rows::Int, cols::Int)
    #number of cols -> width, number of rows -> height
@@ -230,15 +221,15 @@ function print_frame(frame::Array,rows::Int, cols::Int)
    return nothing
 end
 
-x_rotation = [1     0       0   ;
+const x_rotation = [1     0       0   ;
               0   cos(α)  sin(α);
               0   -sin(α) cos(α)]
 
-y_rotation = [cos(β) 0 sin(β);
+const y_rotation = [cos(β) 0 sin(β);
                 0    1   0   ;
              -sin(β) 0 cos(β)]
 
-z_rotation = [cos(δ) sin(δ) 0;
+const z_rotation = [cos(δ) sin(δ) 0;
              -sin(δ) cos(δ) 0;
                 0      0    1]
 
